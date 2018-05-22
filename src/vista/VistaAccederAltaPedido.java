3674ebe466395681c7bf915d8bd15edb1a12d0fb
manufuentes98 <36626964+manufuentes98@users.noreply.github.com>
2018-05-22 12:23:46 +0200

    Botonacos de AccederAltaPedido y AccederPedido (ง ° ͜ ʖ °)ง

--- src/vista/VistaAccederAltaPedido.java
+++ src/vista/VistaAccederAltaPedido.java
@@ -1,9 +1,20 @@
 package vista;
 
 import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 
-public class VistaAccederAltaPedido {
+public class VistaAccederAltaPedido extends JPanel {
+	public VistaAccederAltaPedido() {
+		setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JButton btnNewButton = new JButton("Alta Pedido");
+		add(btnNewButton);
+	}
 
   public JPanel VistaAccederAltaPedido;
-
 }
