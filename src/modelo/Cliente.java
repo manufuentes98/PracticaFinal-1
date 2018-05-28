1d41f0e9f482ac5dabeaf266721c3f72e18a16ed
OscarPS11 <oscarps11yt@gmail.com>
2018-05-28 09:54:10 +0200

    modelo

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,16 +1,62 @@
 package modelo;
-import java.util.Vector;
 
-public class Cliente {
+import java.io.Serializable;
 
-  public String Identificacion;
+public class Cliente implements Serializable {
+	private String dniCif;
+	private String razonSocial;
+	private String direccion;
+	private String telefono;
 
-  public String Nombre;
+	public Cliente(String dniCif, String razonSocial, String direccion, String telefono) {
+		super();
+		this.dniCif = dniCif;
+		this.razonSocial = razonSocial;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
 
-  public String Direccion;
+	public String getDireccion() {
+		return direccion;
+	}
 
-  public int Telefono;
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 
-    public Vector  myLogica;
+	public String getTelefono() {
+		return telefono;
+	}
 
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDniCif() {
+		return dniCif;
+	}
+
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Cliente cliente = (Cliente) obj;
+		boolean retorno = super.equals(obj);
+		if(!retorno){
+			retorno = this.dniCif.equals(cliente.dniCif);
+		}
+		return retorno;
+	}
+
+	@Override
+	public int hashCode() {
+		return dniCif.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return razonSocial;
+	}
 }
