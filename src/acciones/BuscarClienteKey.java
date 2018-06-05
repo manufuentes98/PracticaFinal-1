72ad1537b60fbc7c623fdcbe8efd36181c59f7c1
OscarPS11 <oscarps11yt@gmail.com>
2018-06-05 08:50:17 +0200

    buscarCliente done

--- src/acciones/BuscarClienteKey.java
+++ src/acciones/BuscarClienteKey.java
@@ -1,45 +1,57 @@
 package acciones;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
 import control.Logica;
 import control.Puente;
 import modelo.Cliente;
+import modelo.Datos;
 import utiles.Utiles;
+import vista.VistaEjecutarBuscarArticulo;
 import vista.VistaEjecutarBuscarCliente;
 
-public class BuscarCliente implements ActionListener {
+public class BuscarClienteKey implements KeyListener {
 
 	public Puente puente;
-	BuscarClienteKey buscarClienteKey;
+	private String cadena;
 
-	public BuscarCliente(Puente puente) {
+	public BuscarClienteKey(Puente puente) {
+		super();
 		this.puente = puente;
-
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		buscarClienteKey = new BuscarClienteKey(puente);
+	public void keyTyped(KeyEvent e) {
 		VistaEjecutarBuscarCliente vista = puente.getVistaAccederBuscarCliente().getVistaEjecutarBuscarCliente();
-		vista.getTextField().addKeyListener(this.buscarClienteKey);
-		
-		ArrayList<Cliente> clientes = this.puente.getLogica().getDatos().getClientes();
+		cadena = vista.getTextField().getText();
+		System.out.println(cadena);
+
 		while (this.puente.getModeloTabla().getRowCount() > 0) {
 			this.puente.getModeloTabla().removeRow(0);
-
 		}
+
+		ArrayList<Cliente> clientes = this.puente.getLogica().getDatos().getClientes();
+
 		for (int i = 0; i < clientes.size(); i++) {
 			Cliente clienteAux = this.puente.getLogica().getDatos().getClientes().get(i);
 			String adicion[] = { clienteAux.getRazonSocial(), clienteAux.getDniCif(), clienteAux.getDireccion(),
 					clienteAux.getTelefono() };
-			// addrow al default
-			this.puente.getModeloTabla().addRow(adicion);
-			Utiles.actualizar(puente);
+			if (adicion[0].toLowerCase().startsWith(cadena.toString())) {
+				this.puente.getModeloTabla().addRow(adicion);
+			}
+
 		}
 
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 	}
 
 }
