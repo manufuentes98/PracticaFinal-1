a9f1475fc43d187a1e7de8b4b3fea0d1e26f886b
OscarPS11 <oscarps11yt@gmail.com>
2018-06-05 09:40:20 +0200

    buscarArticulo planteado

--- src/acciones/BuscarArticulo.java
+++ src/acciones/BuscarArticulo.java
@@ -2,25 +2,49 @@ package acciones;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
-import control.Logica;
+import java.util.ArrayList;
 import control.Puente;
+import modelo.Cliente;
+import vista.VistaEjecutarBuscarArticulo;
 
-public class BuscarArticulo implements KeyListener{
+public class BuscarArticulo implements KeyListener {
 
-  public Logica logica ;
+	public Puente puente;
+	private String cadena;
 
-  public Puente puente ;
+	public BuscarArticulo(Puente puente) {
+		super();
+		this.puente = puente;
+	}
 
-@Override
-public void keyPressed(KeyEvent e) {}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		VistaEjecutarBuscarArticulo vista = puente.getVistaEjecutarBuscarArticulo();
+		cadena = vista.getTextBuscarBuscarArticulo().getText();
+		System.out.println(cadena);
 
-@Override
-public void keyReleased(KeyEvent e) {}
+		while (this.puente.getVistaEjecutarBuscarArticulo().getModeloTablaArticulo().getRowCount() > 0) {
+			this.puente.getVistaEjecutarBuscarArticulo().getModeloTablaArticulo().removeRow(0);
+		}
 
-@Override
-public void keyTyped(KeyEvent e) {
-//	TODO busca cuando escriba algo
-}
+		/*ArrayList<Cliente> clientes = this.puente.getLogica().getDatos().getClientes();
 
+		for (int i = 0; i < clientes.size(); i++) {
+			Cliente clienteAux = this.puente.getLogica().getDatos().getClientes().get(i);
+			String adicion[] = { clienteAux.getRazonSocial(), clienteAux.getDniCif(), clienteAux.getDireccion(),
+					clienteAux.getTelefono() };
+			if (adicion[0].toLowerCase().startsWith(cadena.toString())) {
+				this.puente.getModeloTabla().addRow(adicion);
+			}
+
+		}*/
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
 }
